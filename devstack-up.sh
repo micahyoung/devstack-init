f224ae5e8c2c0fce99d5e912cf56cdf27c6d4951
Micah Young <myoung@pivotal.io>
2017-03-28 17:31:06 -0400

    Increate devstack vm memory

--- devstack-up.sh
+++ devstack-up.sh
@@ -4,7 +4,7 @@ dom_name=devstack-0
 if virsh list --all | grep $dom_name; then
   virsh start $dom_name
 else
-  uvt-kvm create --cpu=4 --memory=40960 --disk=200 --run-script-once=devstack-vm.sh $dom_name arch=amd64 release=xenial
+  uvt-kvm create --cpu=4 --memory=51200 --disk=200 --run-script-once=devstack-vm.sh $dom_name arch=amd64 release=xenial
 fi
 while ! uvt-kvm ip $dom_name; do sleep 1; done
 virsh attach-interface $dom_name network bosh --mac de:ad:be:ef:00:02 --model virtio
