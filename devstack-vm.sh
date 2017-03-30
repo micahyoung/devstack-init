c4d6783975da6ddc1737b274bb4d4ad6f506d326
Micah Young <myoung@pivotal.io>
2017-03-29 21:41:59 -0400

    Install htop

--- devstack-vm.sh
+++ devstack-vm.sh
@@ -36,7 +36,7 @@ export no_proxy="127.0.0.1,localhost,$host_ip,$proxy_ip"
 GIT_BASE="https://github.com"
 
 DEBIAN_FRONTEND=noninteractive sudo apt-get -qqy update || sudo yum update -qy
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -qqy git || sudo yum install -qy git
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -qqy git htop || sudo yum install -qy git htop
 
 sudo chown stack:stack /home/stack
 cd /home/stack
