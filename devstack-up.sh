d18b900ac10cb4431305d1edbdf4b4a265457c69
Micah Young <micah.young@gmail.com>
2017-04-12 17:14:15 -0400

    Update devstack-up.sh
    Extract memory and disk to variables for legibility

--- devstack-up.sh
+++ devstack-up.sh
@@ -2,6 +2,8 @@
 
 dom_name=devstack-0
 script=devstack-vm.sh
+memory_mb=$(expr  56 \* 1024)
+disk_gb=300
 if virsh list --all | grep $dom_name; then
   virsh start $dom_name
 else
@@ -11,7 +13,7 @@ $(tar cJ $script | base64 -w0)
 EOF2
 ./$script $(echo $*)
 EOF
-  uvt-kvm create --cpu=4 --memory=57344 --disk=300 --run-script-once=uvt-wrapper.sh $dom_name arch=amd64 release=xenial
+  uvt-kvm create --cpu=4 --memory=$memory_mb --disk=$disk_gb --run-script-once=uvt-wrapper.sh $dom_name arch=amd64 release=xenial
 fi
 while ! uvt-kvm ip $dom_name; do sleep 1; done
 virsh attach-interface $dom_name network bosh --mac de:ad:be:ef:00:02 --model virtio
